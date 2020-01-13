43f5b98612be1230c1ad83859b671ac3d63d089d
Jeroen-editing <jdv.editing@gmail.com>
2020-01-13 02:42:22 +0100

    fixing minutes js and styling clock

--- javascript30_wes_bos/02 - JS and CSS Clock/clock.js
+++ javascript30_wes_bos/02 - JS and CSS Clock/clock.js
@@ -1,11 +1,18 @@
-const secondHand = document.querySelector('.second-hand')
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.minute-hand');
 
 function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
     const secondsDegrees = Math.floor((seconds / 60) * 360);
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-    console.log(secondsDegrees)
+    console.log(secondsDegrees);
+
+    const minutes = now.getMinutes();
+    const minutesDegrees = Math.floor((minutes/ 600) * 360);
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    console.log(minutesDegrees);
 }
 
 setInterval(setDate, 1000);
+
