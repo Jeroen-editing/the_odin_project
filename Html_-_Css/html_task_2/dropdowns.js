efb7e653c9dcceb9dd1db126ecc310bfe3ed8492
Jeroen-editing <jdv.editing@gmail.com>
2020-05-28 09:37:27 +0200

    comments and dropdowns added

---
+++ Html_-_Css/html_task_2/dropdowns.js
@@ -0,0 +1,59 @@
+// ***********************   TABLE   ******************************************
+
+let btnTable = document.getElementById('table-button');
+
+function dropdownTable () {
+    let table = document.getElementById('hidden-table')
+
+    if (table.className.indexOf("table-show") == -1) {
+        table.className += " table-show";
+        btnTable.innerText = "";
+        btnTable.innerText = "Show less";
+
+    } else {
+        table.className = table.className.replace(" table-show", "");
+        btnTable.innerText = "";
+        btnTable.innerText = "Show more";
+    }
+}
+
+btnTable.addEventListener('click', dropdownTable);
+
+
+
+// ***********************   MENU   ******************************************
+
+let btnMenu = document.getElementById('sort-button');
+
+function dropdownMenu () {
+    let menu = document.getElementById('hidden-menu');
+
+    if (menu.className.indexOf("menu-show") == -1) {
+        menu.className += " menu-show";
+
+    } else {
+        menu.className = menu.className.replace(" menu-show", "");
+    }
+}
+
+btnMenu.addEventListener('click', dropdownMenu);
+
+
+// ***********************   SIGNI IN & SUBSCRIBE   ******************************************
+
+let btnSubscribeSignIn = document.getElementById('subscribe-signin-button');
+
+
+function dropdownSubscribeSignIn () {
+    let subscribeSignIn = document.getElementById('hidden-subscribe-sigin');
+
+
+    if (subscribeSignIn.className.indexOf("subscribe-sigin-show") == -1) {
+        subscribeSignIn.className += " subscribe-sigin-show";
+
+    } else {
+        subscribeSignIn.className = subscribeSignIn.className.replace(" subscribe-sigin-show", "");
+    }
+}
+
+btnSubscribeSignIn.addEventListener('click', dropdownSubscribeSignIn);
