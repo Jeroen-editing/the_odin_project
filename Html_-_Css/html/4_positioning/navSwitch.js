6b078e47f334283b241f98d17fb666c99456cd4b
Jeroen-editing <jdv.editing@gmail.com>
2020-07-03 07:25:56 +0200

    javascript textoverflows with scrolling

--- Html_-_Css/html/4_positioning/navSwitch.js
+++ Html_-_Css/html/4_positioning/navSwitch.js
@@ -1,3 +1,4 @@
+/* POP-UP NAV */
 /* Calculate height of image and set switchpoi nav at 2/3 of height */
 
 let switchMark = document.querySelector(".mainImage");
@@ -6,66 +7,107 @@ let bottomOfImage = Math.round(switchMark.offsetTop + 2 * (switchMark.height/3))
 
 
 /* ------- LEFTNAV variables--------------------------------------- */
-
 let navLeftFirst = document.getElementById("navLeftOrigenal");
 let navLeftSecond = document.getElementById("navLeftReplace");
 
 /* ------- MIDDLENAV variables------------------------------------- */
-
 let navMiddleFirst = document.getElementById("navMiddleOrigenal");
 let navMiddleSecond = document.getElementById("navMiddleReplace");
 
 /* ------- RIGHTNAV variables-------------------------------------- */
-
 let navRightFirst = document.getElementById("navRightOrigenal");
 let navRightSecond = document.getElementById("navRightReplace");
 
 
-function switchNav() {
-    
-    console.log(bottomOfImage, window.scrollY);
+function switchNav() {   
+    /*console.log(bottomOfImage, window.scrollY);*/
 
     if (window.scrollY >= bottomOfImage) {
 
         /* ------- LEFTNAV classSwitch ----------------------------- */
-
         navLeftFirst.classList.replace("navLeft", "hiddenLeftNav");
-
         navLeftSecond.classList.replace("hiddenLeftNav", "replacedLeftNav");
 
         /* ------- MIDDELNAV classSwitch --------------------------- */
-
         navMiddleFirst.classList.replace("navMiddle", "hiddenMiddleNav");
-
         navMiddleSecond.classList.replace("hiddenMiddleNav", "replacedMiddleNav");
 
-        /* ------- RIGHTNAV classSwitch ---------------------------- */
-        
+        /* ------- RIGHTNAV classSwitch ---------------------------- */     
         navRightFirst.classList.replace("navRight", "hiddenRightNav");
-
         navRightSecond.classList.replace("hiddenRightNav", "replacedRightNav");
 
     } else {
 
         /* ------- LEFTNAV classSwitch ----------------------------- */
-
         navLeftFirst.classList.replace("hiddenLeftNav", "navLeft");
-
         navLeftSecond.classList.replace("replacedLeftNav", "hiddenLeftNav");
 
         /* ------- MIDDELNAV classSwitch --------------------------- */
-
         navMiddleFirst.classList.replace("hiddenMiddleNav", "navMiddle");
-
         navMiddleSecond.classList.replace("replacedMiddleNav", "hiddenMiddleNav");
 
         /* ------- RIGHTNAV classSwitch ---------------------------- */
-
         navRightFirst.classList.replace("hiddenRightNav", "navRight");
-
         navRightSecond.classList.replace("replacedRightNav", "hiddenRightNav");
-
     }
-}
+};
 
-window.addEventListener('scroll', switchNav);
+document.addEventListener('scroll', switchNav);
+
+
+
+/* POP-UP TEXT on images */
+
+let banner_One = document.querySelector(".bannerOne");
+let banner_Two = document.querySelector(".bannerTwo");
+let banner_Three = document.querySelector(".bannerThree");
+
+let textOverflowOne = document.getElementById("textPopUpOne");
+let textOverflowTwo = document.getElementById("textPopUpTwo");
+let textOverflowThree = document.getElementById("textPopUpThree");
+
+function textShown() {
+    let top = banner_Two.offsetTop - 600;
+    let bottom = banner_Two.offsetTop - 150;
+    let numbers = "top:" + top + "/ bottom: " + bottom + "/ y: " + scrollY;
+
+    console.log(numbers);
+    
+    if (window.scrollY > (banner_One.offsetTop - 600) && window.scrollY < (banner_One.offsetTop - 150)) {
+
+        textOverflowOne.classList.replace("textoverflow_hidden", "textoverflow_shown");
+
+    } else {
+
+        textOverflowOne.classList.replace("textoverflow_shown", "textoverflow_flowOut");
+        setTimeout(function () {
+            textOverflowOne.classList.replace("textoverflow_flowOut", "textoverflow_hidden");
+        }, 3000);
+    }
+    
+    if (window.scrollY > (banner_Two.offsetTop - 600) && window.scrollY < (banner_Two.offsetTop - 150)) {
+
+        textOverflowTwo.classList.replace("textoverflow_hidden", "textoverflow_shown");
+
+    } else {
+
+        textOverflowTwo.classList.replace("textoverflow_shown", "textoverflow_flowOut");
+        setTimeout(function () {
+            textOverflowTwo.classList.replace("textoverflow_flowOut", "textoverflow_hidden");
+        }, 3000);
+    }
+    
+    if (window.scrollY > (banner_Three.offsetTop - 600) && window.scrollY < (banner_Three.offsetTop - 150)) {
+
+        textOverflowThree.classList.replace("textoverflow_hidden", "textoverflow_shown");
+
+    } else {
+
+        textOverflowThree.classList.replace("textoverflow_shown", "textoverflow_flowOut");
+        setTimeout(function () {
+            textOverflowThree.classList.replace("textoverflow_flowOut", "textoverflow_hidden");
+        }, 3000);
+    }
+};
+
+document.addEventListener('scroll', textShown);
