c76c17a4444ccf076c561637c57e00e309394fcf
Jeroen-editing <jdv.editing@gmail.com>
2020-07-18 09:48:43 +0200

    started and working fizzbuzz

---
+++ JavaScript/fizzbuzz/Solving.md
@@ -0,0 +1,40 @@
+Fizz Buzz
+
+
+Write a program that prints each number 
+betweeen 1 and 
+the given number => entered by the user.
+
+For the numbers that divide by 3 (without a remainder)
+it prints 'Fizz' instead.
+For the numbers that divide by 5 (without a remainder)
+it prints 'Buzz' instead.
+For the numbers that divide by 3 and 5 (without a remainder)
+it prints 'FizzBuzz' instead.
+
+
+
+Task is nomaly for console only.
+But I want a user interface.
+
+So I need a form with an input (number) and a submit button.
+
+
+I also want the result in a nice interface under the form.
+
+
+Desired output =  a list of numbers from 1 to the 'given number and
+each number that divide by 3 wil be replaced by 'Fizz',
+each number that divide by 5 wil be replaced by 'Buzz' and
+each number that divide by 3 & 5 wil be replaced by 'FizzBuzz'.
+
+
+Pseudo Code:
+
+When a user inputs a number Loop from 1 to the entered number If the current number is divisible by 3 then print "Fizz" If the current number is divisible by 5 then print "Buzz" If the current number is divisible by 3 and 5 then print "FizzBuzz" Otherwise print the current number.
+
+
+First step:
+
+create a possible input and see it's an integer.
+so make a form for it and see it's submitted (to the js-file)
---
+++ JavaScript/fizzbuzz/fizzbuzz.js
@@ -0,0 +1,56 @@
+let given_number = document.getElementById("givenNumber");
+
+let submit_btn = document.getElementById("submit");
+
+let outputScreen = document.getElementById("output");
+
+let output = ['<p>'];
+
+let fizz = "<span class='fizz'>fizz";
+
+let buzz = "<span class='buzz'>buzz";
+
+let fizzbuzz = "<span class='fizzbuzz'>fizzbuzz";
+
+
+function showNumber() {
+
+    let number = Number(given_number.value);
+
+    console.log(number + " => type = " + typeof number);
+
+    for (let index = 1; index <= number; index++) {
+
+        if (index%3 === 0 && index%5 === 0) {
+            output += fizzbuzz + ', </span>';
+        } else if (index%3 === 0) {
+            output += fizz + ', </span>';
+        } else if (index%5 === 0) {
+            output += buzz + ', </span>';
+        } else if (index < number) {
+            output += '<em>' + index + ',</em> ';
+        }
+        /*
+        if (index === number ) {
+            
+            if (index%3 === 0 && index%5 === 0) {
+                output += fizzbuzz + '.';
+            } else if (index%3 === 0) {
+                output += fizz + '.';
+            } else if (index%5 === 0) {
+                output += buzz + '.';
+            } else {
+                output += '<em>' + index + '.</em></p>';
+            }
+        }
+        */
+        if (index%10 === 0) {
+            output += '</p><p>'
+        }
+    }
+
+    outputScreen.innerHTML = output;
+}
+
+
+submit_btn.addEventListener("click", showNumber);
